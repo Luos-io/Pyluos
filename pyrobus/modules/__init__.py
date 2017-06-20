8615fecc41ae4f22ed6bcaeb9bedf3ffcd6bc527
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-06-20 11:58:40 +0200

    Add multiple registers for dynamixel module :
    - W goal_position
    - W moving_speed
    - W compliant
    - R present_position

--- pyrobus/modules/__init__.py
+++ pyrobus/modules/__init__.py
@@ -1,17 +1,17 @@
 from .distance import Distance
+from .dxl import Dynamixel
 from .button import Button
 from .potard import Potard
 from .servo import Servo
 from .relay import Relay
-from .dxl import DxlBus
 from .led import Led
 
 
 __all__ = [
     'name2mod',
+    'Dynamixel',
     'Distance',
     'Button',
-    'DxlBus',
     'Potard',
     'Servo',
     'Relay',
@@ -19,8 +19,8 @@ __all__ = [
 ]
 
 name2mod = {
+    'dynamixel': Dynamixel,
     'distance': Distance,
-    'dynamixel': Servo,
     'button': Button,
     'potard': Potard,
     'servo': Servo,
--- pyrobus/modules/dxl.py
+++ pyrobus/modules/dxl.py
@@ -1,89 +1,70 @@
-from collections import defaultdict
+from __future__ import division
+
+from ipywidgets import interact
 
 from .module import Module
 
 
-class DxlBus(Module):
-    possible_events = {'moved'}
-
+class Dynamixel(Module):
     def __init__(self, id, alias, robot):
         Module.__init__(self, 'Dynamixel', id, alias, robot)
 
-        self._pos = self._speed = None
+        self._present_position = None
+        self._goal_position = None
+        self._moving_speed = None
+        self._compliant = None
 
-        self._reg = defaultdict(lambda: None)
-
-        self.goal_position = 0
-        self.moving_speed = 0
-        self.torque_limit = 100
-        self.compliant = False
+        self.goal_position = 90
+        self.moving_speed = 100
+        self.compliant = True
 
     @property
     def present_position(self):
-        return self._pos
+        return self._present_position
 
     @property
-    def present_speed(self):
-        return self._speed
+    def position(self):
+        return self.present_position
 
     @property
     def goal_position(self):
-        return self._reg['goal']
+        return self._goal_position
 
     @goal_position.setter
-    def goal_position(self, new_goal):
-        if new_goal != self._reg['goal']:
-            self._reg['goal'] = new_goal
-            self._push_value('goal_position', new_goal)
+    def goal_position(self, new_pos):
+        if new_pos != self._goal_position:
+            self._goal_position = new_pos
+            self._push_value('goal_position', self._goal_position)
 
     @property
     def moving_speed(self):
-        return self._reg['speed']
+        return self._moving_speed
 
     @moving_speed.setter
     def moving_speed(self, new_speed):
-        if new_speed != self._reg['speed']:
-            self._reg['speed'] = new_speed
-            self._push_value('moving_speed', new_speed)
-
-    @property
-    def torque_limit(self):
-        return self._reg['torque']
-
-    @torque_limit.setter
-    def torque_limit(self, new_torque):
-        if new_torque != self._reg['torque']:
-            self._reg['torque'] = new_torque
-            self._push_value('torque_limit', new_torque)
+        if new_speed != self._moving_speed:
+            self._moving_speed = new_speed
+            self._push_value('moving_speed', self._moving_speed)
 
     @property
     def compliant(self):
-        return self._reg['compliant']
+        return self._compliant == 1
 
     @compliant.setter
     def compliant(self, new_compliancy):
-        if new_compliancy != self._reg['compliant']:
-            self._reg['compliant'] = new_compliancy
-            self._push_value('compliant', new_compliancy)
+        if new_compliancy != self._compliant:
+            self._compliant = 1 if new_compliancy else 0
+            self._push_value('compliant', self._compliant)
 
     def _update(self, new_state):
-        if 'position' in new_state:
-            new_pos = new_state['position']
+        new_pos = new_state['present_position']
 
-            if new_pos != self._pos:
-                self._pub_event('moved', self._pos, new_pos)
-                self._pos = new_pos
+        if new_pos != self._present_position:
+            self._pub_event('moved', self._present_position, new_pos)
+            self._present_position = new_pos
 
-        if 'speed' in new_state:
-            new_speed = new_state['speed']
+    def control(self):
+        def move(position):
+            self.position = position
 
-            if new_speed != self._speed:
-                self._speed = new_speed
-
-    def _state_repr(self):
-        state = ''
-        if self._pos is not None:
-            state += 'position={}'.format(self._pos)
-        if self._speed is not None:
-            state += ' speed={}'.format(self._speed)
-        return state
+        return interact(move, position=(-180, 180, 1))

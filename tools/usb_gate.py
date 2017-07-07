dda35fa5903fdce87ff3a5c3474bde6d11ae0111
Pierre Rouanet <pierre.rouanet@gmail.com>
2017-07-07 16:01:29 +0200

    Add utilities for the USB gates:
    - discovery (using a white list)
    - redirect a USB gate to a Websocket

---
+++ tools/usb_gate.py
@@ -0,0 +1,39 @@
+from subprocess import Popen
+
+from serial.tools.list_ports import comports
+
+white_list = {
+    # Add here the USB gates you want to automatically be used
+    # name: serial-port
+    'my_usb_gate1': '/dev/cu.usbmodem3203731',
+}
+
+
+def discover():
+    gates = [p.device for p in comports()]
+    return {
+        alias: port
+
+        for (alias, port) in white_list.items()
+        if port in gates
+    }
+
+
+def redirect_to_ws(serial_port, ws_port):
+    return Popen(['python', 'usb2ws.py',
+                  '--serial-port', serial_port,
+                  '--ws-port', str(ws_port)])
+
+
+if __name__ == '__main__':
+    import time
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cmd', choices=('discover', ))
+    args = parser.parse_args()
+
+    if args.cmd == 'discover':
+        while True:
+            print(discover())
+            time.sleep(1.0)

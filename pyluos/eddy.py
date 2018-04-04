2c8e5df91295a9b1966f2192f2517192fdc86c01
Pierre Rouanet <pierre.rouanet@gmail.com>
2018-04-04 11:32:17 +0200

    Bugfix in light sensors for eddy.

--- pyluos/eddy.py
+++ pyluos/eddy.py
@@ -13,5 +13,6 @@ class Eddy(object):
 
         self.left_wheel = self._robot.eddy.left_wheel
         self.right_wheel = self._robot.eddy.right_wheel
-        self.left_light_sensor = self._robot.eddy.left_light_sensor
-        self.right_light_sensor = self._robot.eddy.right_light_sensor
+
+        self.left_light_sensor = self._robot.eddy.right_light_sensor
+        self.right_light_sensor = self._robot.eddy.left_light_sensor

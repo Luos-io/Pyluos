0c35f9da0619a8122768c7079c59ca4dc1cd01cf
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2018-08-03 12:49:55 +0200

    fix pin reception

--- pyluos/modules/l0_gpio.py
+++ pyluos/modules/l0_gpio.py
@@ -19,11 +19,15 @@ class L0GPIO(Module):
         self.analog_9 = AnalogInputPin('p9')
 
     def _update(self, new_state):
-        self.digital_5._update(new_state['p5'])
-        self.digital_6._update(new_state['p6'])
-        self.analog_7._update(new_state['p7'])
-        self.analog_8._update(new_state['p8'])
-        self.analog_9._update(new_state['p9'])
+        Module._update(self, new_state)
+
+        if 'p1' in new_state:
+            self.analog_1._update(new_state['p1'])
+            self.digital_5._update(new_state['p5'])
+            self.digital_6._update(new_state['p6'])
+            self.analog_7._update(new_state['p7'])
+            self.analog_8._update(new_state['p8'])
+            self.analog_9._update(new_state['p9'])
 
     # notebook things
     def control(self):

a7caab7f8ab5d357b6666daebed252016ab8a28c
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2018-08-15 10:36:39 +0200

    manage servo

--- pyluos/modules/servo.py
+++ pyluos/modules/servo.py
@@ -8,26 +8,20 @@ class Servo(Module):
         Module.__init__(self, 'Servo', id, alias, robot)
 
     @property
-    def target_position(self):
+    def position(self):
         return self._value
 
-    @target_position.setter
-    def target_position(self, new_pos):
+    @position.setter
+    def position(self, new_pos):
         if new_pos != self._value:
             self._value = new_pos
             self._push_value('target_position', new_pos)
 
-    @property
-    def target_speed(self):
-        return self.target_position / 180 * 200 - 100
-
-    @target_speed.setter
-    def target_speed(self, new_speed):
-        pos = (new_speed + 100) / 200 * 180
-        self.target_position = pos
+    def _update(self, new_state):
+        Module._update(self, new_state)
 
     def control(self):
         def move(position):
-            self.target_position = position
+            self.position = position
 
         return interact(move, position=(0, 180, 1))

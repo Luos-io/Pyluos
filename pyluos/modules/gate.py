8cb06f4d07339c675a09826b19e52c364535b128
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2019-03-26 14:35:35 +0100

    manage delay command on gate

--- pyluos/modules/gate.py
+++ pyluos/modules/gate.py
@@ -8,3 +8,19 @@ class Gate(Module):
 
     def _update(self, new_state):
         Module._update(self, new_state)
+
+    @property
+    def delay_ms(self):
+        self._value
+
+    @delay_ms.setter
+    def delay_ms(self, s):
+        if s != self._value:
+            self._value = s
+            self._push_value("delay",s)
+
+    def control(self):
+        def delay(delay):
+            self._value = delay
+
+        return interact(delay, delay=(0, 100, 1))

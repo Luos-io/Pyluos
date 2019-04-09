a4e9f54254686e6fc82a19e98ff58c0364ad0050
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2019-04-09 17:22:46 +0200

    manage transition time for led

--- pyluos/modules/led.py
+++ pyluos/modules/led.py
@@ -5,6 +5,7 @@ class Led(Module):
     def __init__(self, id, alias, robot):
         Module.__init__(self, 'LED', id, alias, robot)
         self.color = (0, 0, 0)
+        self.time = 0.0
 
     @property
     def color(self):
@@ -17,6 +18,15 @@ class Led(Module):
         if new_color != self._value:
             self._value = new_color
             self._push_value('color', new_color)
+
+    @property
+    def time(self):
+        return self._time
+
+    @time.setter
+    def time(self, new_time):
+            self._time = new_time
+            self._push_value('time', new_time)
 
     def _update(self, new_state):
         Module._update(self, new_state)

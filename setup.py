b644cb16a53e04fc7992b971879fff6894dd61da
Nicolas Rabault <nicolas.rabault@luos-robotics.com>
2020-04-08 14:42:02 +0200

    Add pyluos pip informations, fix #64

--- setup.py
+++ setup.py
@@ -6,9 +6,17 @@ from setuptools import setup, find_packages
 
 version = imp.load_source('pyluos.version', 'pyluos/version.py')
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='pyluos',
       version=version.version,
+      author="Luos",
+      author_email="hello@luos.io",
+      url="https://docs.luos.io/pages/high/pyluos.html",
+      description="Python library to set the high level behavior of your device based on Luos embedded system.",
+      long_description=long_description,
+      license='MIT',
       packages=find_packages(),
       install_requires=['future',
                         'websocket-client',

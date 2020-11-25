614d539db382ccd6cca48be81f68a342938c4691
Nicolas Rabault <nicolas.rabault@luos.io>
2020-11-25 11:34:32 +0100

    update satatistics JSON API management

--- pyluos/containers/container.py
+++ pyluos/containers/container.py
@@ -42,7 +42,7 @@ class Container(object):
         self._uuid = [0, 0, 0]
         self._killed = False
         self._last_update = time.time()
-        self._luos_statistics = [0, 0, 0, 0, 0]
+        self._luos_statistics = {}
 
     def __repr__(self):
         return ('<{self.type} '
@@ -103,15 +103,14 @@ class Container(object):
     def luos_statistics(self):
         self._push_value('luos_statistics', "")
         time.sleep(0.3)
-        max_table = [self._luos_statistics[0], self._luos_statistics[1], self._luos_statistics[2]]
+        max_table = [self._luos_statistics["msg_stack"], self._luos_statistics["luos_stack"]]
         max_val = max(max_table)
         s = self.alias + " statistics :"
         s = s + "\n.luos allocated RAM occupation \t= " + repr(max_val)
-        s = s + "%\n\t.Allocator stack \t= " + repr(self._luos_statistics[0])
-        s = s + "%\n\t.Message stack \t\t= " + repr(self._luos_statistics[1])
-        s = s + "%\n\t.Luos stack \t\t= " + repr(self._luos_statistics[2])
-        s = s + "%\n.Dropped messages number \t= " + repr(self._luos_statistics[3])
-        s = s + "\n.Max luos loop delay \t\t= " + repr(self._luos_statistics[4]) + "ms"
+        s = s + "%\n\t.Message stack \t\t= " + repr(self._luos_statistics["msg_stack"])
+        s = s + "%\n\t.Luos stack \t\t= " + repr(self._luos_statistics["luos_stack"])
+        s = s + "%\n.Dropped messages number \t= " + repr(self._luos_statistics["msg_drop"])
+        s = s + "\n.Max luos loop delay \t\t= " + repr(self._luos_statistics["loop_ms"]) + "ms"
         print(s)
 
     def rename(self, name):

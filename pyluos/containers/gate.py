273375b4aec95d0594acfce3d8bac323833fd930
Nicolas Rabault <nicolas.rabault@luos.io>
2021-05-18 11:17:23 +0200

    Remove delay_ms on gate

--- pyluos/containers/gate.py
+++ pyluos/containers/gate.py
@@ -9,15 +9,6 @@ class Gate(Container):
     def _update(self, new_state):
         Container._update(self, new_state)
 
-    @property
-    def delay_ms(self):
-        self._value
-
-    @delay_ms.setter
-    def delay_ms(self, s):
-        self._value = s
-        self._push_value("delay",s)
-
     def control(self):
         def delay(delay):
             self._value = delay

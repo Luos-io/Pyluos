0db1d8bd50a092f79d11c26c62cb15a8d9eaa13e
nicolas cros <nicolas.cros@luos.io>
2021-07-27 11:06:52 +0200

    add crc8 module in dependencies list

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(name='pyluos',
                         'zeroconf',
                         'numpy',
                         'anytree',
+                        'crc8',
                         ],
       extras_require={
           'tests': ['pytest', 'flake8'],

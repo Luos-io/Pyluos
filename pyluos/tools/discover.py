4d1f9eac7fdbc038e5863e2517a3102b17fefef4
JeromeGalan <jerome.galan@luos.io>
2022-10-18 11:29:11 +0200

    Drop void messages in serial discover

--- pyluos/tools/discover.py
+++ pyluos/tools/discover.py
@@ -14,26 +14,43 @@ def serial_discover(baudrate=1000000):
     available_serial = []
     print("Searching for a gate available")
     for serial_host in serial_hosts:
-        print("Testing " + str(serial_host))
+        print("Testing port " + str(serial_host))
         try:
-            port = serial.Serial(serial_host, baudrate, timeout=0.05)
+            port = serial.Serial(serial_host, baudrate, timeout=0.2)
+            time.sleep(0.1)
         except:
             continue
 
-        s = b'{}'
-        port.write(b'\x7E' + struct.pack('<H', len(s)) + s + b'\x81')
-        time.sleep(0.01)
-        s = b'{\"discover\": {}}'
-        port.write(b'\x7E' + struct.pack('<H', len(s)) + s + b'\x81')
-        port.flush()
-        for x in range(10):
+        if port is not None:
+            s = b'{}'
+            port.write(b'\x7E' + struct.pack('<H', len(s)) + s + b'\x81')
+            time.sleep(0.01)
+            port.readline()
+            port.flush()
+            time.sleep(0.1)
+            s = b'{\"discover\": {}}'
+            port.write(b'\x7E' + struct.pack('<H', len(s)) + s + b'\x81')
             state = port.readline()
-            if ('gate'.encode() in state):
+            gateResponse = False
+            if 'gate'.encode() in state:
+                gateResponse = True
+            elif b'{}\n' in state:
+                # if many "void" messages are received, drop them and retry reception
+                for read_retry in range(1000):
+                    state = port.readline()
+                    if 'gate'.encode() in state:
+                        print(f'DEBUG {read_retry}')
+                        gateResponse = True
+                        break
+            if gateResponse:
                 available_serial.append(serial_host)
-                continue
+                port.close()
 
-        port.close()
-    return available_serial
+    if available_serial :
+        return available_serial
+    else:
+        print("... No gate detected")
+        return []
 
 def main():
     parser.add_argument("--baudrate", action="store",
